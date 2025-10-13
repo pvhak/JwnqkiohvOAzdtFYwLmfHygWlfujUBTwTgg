c36fb4db0362d43391c2c4f7515fe1171a37e2c6
pvhak <182967368+pvhak@users.noreply.github.com>
2025-10-13 15:11:12 +0300

    Update return.js

--- api/return.js
+++ api/return.js
@@ -1,7 +1,10 @@
-import { std } from "./receive.js"; // look in receive.js to get this.. haha.. im so funny......
-import { cstatus } from "./status.js";
+import cors from "./_cors"; // i hate u cors
+import { std } from "./receive"; // look in receive.js to get this.. haha.. im so funny......
+import { cstatus } from "./status";
 
 export default function handler(req, res) {
+  cors(res);
+  if (req.method === "OPTIONS") return res.status(200).end();
   const { key } = req.query;
   if (key !== "test") {return res.status(403).json({ error: "nooo" });}
   res.setHeader("Content-Type", "text/plain");
