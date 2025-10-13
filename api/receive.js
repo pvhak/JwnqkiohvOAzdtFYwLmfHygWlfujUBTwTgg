2476ca1036e11bd96fa4c49d779ac3fc6b5c17e0
pvhak <182967368+pvhak@users.noreply.github.com>
2025-10-13 15:23:08 +0300

    Update receive.js

--- api/receive.js
+++ api/receive.js
@@ -1,19 +1,18 @@
 import cors from "./_cors";
-let std = ""; // haha.. get it? STD? like.. std?? Hahahha im so funny right? right???
+import { setstd } from "../lib/shared.js";
+// (i had to remove it) haha.. get it? STD? like.. std?? Hahahha im so funny right? right???
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   cors(res);
   if (req.method === "OPTIONS") return res.status(200).end();
 
   const { key } = req.query;
-  if (key !== "test") {return res.status(403).json({ error: "nooo" });}
+  if (key !== "test") return res.status(403).json({ error: "nooo" });
   if (req.method !== "POST") return res.status(405).json({ error: "not allowed lil NIGGA" });
 
   const { data } = req.body || {};
   if (!data) return res.status(400).json({ error: "?????" });
 
-  std = data;
-  res.status(200).json({ success: true, received: data });
+  setstd(data);
+  res.status(200).json({ ok: true });
 }
-
-export { std };
