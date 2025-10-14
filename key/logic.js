221db6dad78140ff46ab99da184a0f293801b51b
pvhak <182967368+pvhak@users.noreply.github.com>
2025-10-14 17:15:44 +0300

    Update logic.js

--- key/logic.js
+++ key/logic.js
@@ -1,1 +1,8 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+    const user = document.getElementById("username").value.trim();
+    const pass = document.getElementById("password").value.trim();
 
+    if (!user || !pass) {return;}
+
+    if (user === "admin" && pass === "password") { alert("hi"); } else {alert("no");}
+});
