21de0a4149b29cdfae2b56247e408a83954303c0
pvhak <182967368+pvhak@users.noreply.github.com>
2025-10-14 20:14:23 +0300

    Update logic.js

--- editor/logic.js
+++ editor/logic.js
@@ -1,1 +1,15 @@
+(async () => {
+  const resp = await fetch("/api/getstatus");
+  if (resp.status === 403) window.location.href = "/";
+})();
 
+document.getElementById("clear-btn").addEventListener("click", () => {
+  window.editor.setValue("");
+});
+
+document.getElementById("execute-btn").addEventListener("click", async () => {
+  const code = window.editor.getValue();
+  const resp = await fetch(`/api/ccode?code=${encodeURIComponent(code)}`);
+  const result = await resp.json();
+  console.log(result);
+});
