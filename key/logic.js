228375fa63dfb6997730314a6fa31dfd439129af
pvhak <182967368+pvhak@users.noreply.github.com>
2025-10-14 21:01:51 +0300

    Update logic.js

--- key/logic.js
+++ key/logic.js
@@ -1,9 +1,29 @@
-document.getElementById("login-btn").addEventListener("click", async function () {
-  const user = document.getElementById("username").value.trim();
-  const pass = document.getElementById("password").value.trim();
-  if (!user || !pass) return;
+async function checktsout() {
+  try {
+    const resp = await fetch("/api/getstatus", { credentials: "include" });
+    if (resp.status === 403) return;
+    const data = await resp.json();
+    if (data && typeof data.status !== "undefined") {
+      window.location.href = "/editor";
+    }
+  } catch (err) {console.error("failed to check:", err);}
+}
 
-  const resp = await fetch(`/api/login?user=${encodeURIComponent(user)}&password=${encodeURIComponent(pass)}`);
-  const data = await resp.json();
-  if (data.redirect) {window.location.href = data.redirect;} else {alert("wrong key");}
+window.addEventListener("DOMContentLoaded", () => {
+  checktsout();
+  document.getElementById("login-btn").addEventListener("click", async function () {
+    const user = document.getElementById("username").value.trim();
+    const pass = document.getElementById("password").value.trim();
+    if (!user || !pass) return;
+    try {
+      const resp = await fetch(`/api/login?user=${encodeURIComponent(user)}&password=${encodeURIComponent(pass)}`);
+      const data = await resp.json();
+      if (data.redirect) {
+        window.location.href = data.redirect;
+      } else {alert("wrong key");}
+    } catch (err) {
+      console.error("fail:", err);
+      alert("wrong password or username!");
+    }
+  });
 });
