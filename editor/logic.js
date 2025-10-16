e8d8b9f8fd6eda87ec43397452e52cb546290338
pvhak <182967368+pvhak@users.noreply.github.com>
2025-10-16 14:52:15 +0300

    pfp is now useful

--- editor/logic.js
+++ editor/logic.js
@@ -4,7 +4,7 @@ function updstatus(status) {
   const dot = document.querySelector(".status-dot");
   const statustxt = document.querySelector(".status-text");
 
-  if (!dot || !statustxt) {return;}
+  if (!dot || !statustxt) return;
 
   if (status) {
     dot.style.backgroundColor = "#64F575";
@@ -13,6 +13,11 @@ function updstatus(status) {
     dot.style.backgroundColor = "#e93f40";
     statustxt.textContent = "Not Connected";
   }
+}
+
+function upd_username(username) {
+  const un_span = document.querySelector(".top-right-bar .username");
+  if (un_span && username) { un_span.textContent = username; }
 }
 
 async function poll_status() {
@@ -25,7 +30,7 @@ async function poll_status() {
     }
 
     const data = await resp.json();
-
+    upd_username(data.user);
     if (data.status !== last_status) {
       last_status = data.status;
       updstatus(last_status);
@@ -35,16 +40,20 @@ async function poll_status() {
   }
 }
 
-
 window.addEventListener("DOMContentLoaded", () => {
   poll_status();
   setInterval(poll_status, 2000);
-  document.getElementById("clear-btn")?.addEventListener("click", () => {window.editor.setValue("");});
+
+  document.getElementById("clear-btn")?.addEventListener("click", () => {
+    window.editor.setValue("");
+  });
+
   document.getElementById("execute-btn")?.addEventListener("click", async () => {
     if (!last_status) {
-       notify("Please connect first.", "info", 4000);
-       return;
+      notify("Please connect first.", "info", 4000);
+      return;
     }
+
     const code = window.editor.getValue();
     try {
       const resp = await fetch(`/api/ccode?code=${encodeURIComponent(code)}`, {
@@ -58,6 +67,47 @@ window.addEventListener("DOMContentLoaded", () => {
 
       const result = await resp.json();
       console.log(result);
-    } catch (err) {console.error("failed to exec; ", err);}
+    } catch (err) {
+      console.error("failed to exec; ", err);
+    }
   });
+});
+
+//////////////////////////////////////////////////////////////////////////////
+
+const pfp_frame = document.getElementById("pfp_frame");
+const lo_btn = document.getElementById("lu-btn");
+let ttip_active = false;
+
+pfp_frame.addEventListener("mouseenter", () => {
+  if (!ttip_active) pfp_frame.classList.add("hover");
+});
+pfp_frame.addEventListener("mouseleave", () => {
+  if (!ttip_active) pfp_frame.classList.remove("hover");
+});
+
+pfp_frame.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (ttip_active) {
+    ttip_active = false;
+    pfp_frame.classList.remove("active");
+    pfp_frame.classList.add("hover");
+  } else {
+    ttip_active = true;
+    pfp_frame.classList.add("active");
+    pfp_frame.classList.remove("hover");
+  }
+});
+
+document.addEventListener("click", () => {
+  if (ttip_active) {
+    ttip_active = false;
+    pfp_frame.classList.remove("active");
+    pfp_frame.classList.remove("hover");
+  }
+});
+
+lo_btn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  window.location.href = "/api/logout";
 });
