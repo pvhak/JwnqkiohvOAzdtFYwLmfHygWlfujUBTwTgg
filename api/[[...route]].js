eef47fd11afb5459fc928f1cc3c28f724314bbdd
jiewbcdno <72634292+jiewbcdno@users.noreply.github.com>
2025-11-03 10:48:07 +1100

    noooooooooo

--- api/[[...route]].js
+++ api/[[...route]].js
@@ -8,7 +8,7 @@ let state = global.__STATE__ || {
 
 
 function gen_scriptid() {
-  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789.";
   let id = "";
   for (let i = 0; i < 12; i++) {
     id += chars.charAt(Math.floor(Math.random() * chars.length));
