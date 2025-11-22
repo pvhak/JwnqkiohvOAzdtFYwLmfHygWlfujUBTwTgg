142cbd0b1f8e396823999d15d4a25b5ce53ba249
pvhak <182967368+pvhak@users.noreply.github.com>
2025-11-22 23:01:29 +0200

    Update tabsys.js

--- editor/tabsys.js
+++ editor/tabsys.js
@@ -1,17 +1,21 @@
-const amigooningrn = window.location.pathname.includes("editor"); // Sorry?
+const amigooningrn = window.location.pathname.includes("editor");
 const ebtn = document.getElementById("editor_button");
 const cbtn = document.getElementById("console_button");
 
-// editor tab
 if (amigooningrn) {
     ebtn.classList.add("active");
     ebtn.onclick = null;
-    ebtn.onclick = () => window.location.href = "/console";
+    cbtn.onclick = () => {
+        console.log("console click");
+        window.location.href = "/console";
+    };
 }
 
-// console tab
 else {
     cbtn.classList.add("active");
     cbtn.onclick = null;
-    ebtn.onclick = () => window.location.href = "/editor";
+    ebtn.onclick = () => {
+        console.log("editor click");
+        window.location.href = "/editor";
+    };
 }
