14f863a902482d0266d156314c9a74a1d6856575
pvhak <182967368+pvhak@users.noreply.github.com>
2026-07-21 21:28:00 +0300

    Refactor audio playback start logic and events

--- assets/audio.js
+++ assets/audio.js
@@ -2,19 +2,18 @@ const audio = new Audio("https://8967.lol/assets/fat.mp3");
 audio.loop = true;
 
 let started = false;
+const events = ["click", "pointerdown", "touchstart", "keydown"];
 
 function startNOWWW() {
     if (started) return;
     started = true;
-    audio.play().catch(err => { 
-        console.log("err : ", err); 
-        started = false;
-     });
-    events.forEach(e => window.removeEventListener(e, startNOWWW, true));
+
+    audio.play()
+        .then(() => { events.forEach(e => window.removeEventListener(e, startNOWWW, true));  })
+        .catch(err => {
+            console.log("err:", err);
+            started = false;
+        });
 }
 
-const events = [ "click", "mousedown", "mouseup", "mousemove", "keydown", "keyup", "keypress",
-    "touchstart", "touchend", "pointerdown", "pointerup", "wheel", "scroll"
-];
-
-events.forEach(e => window.addEventListener(e, startNOWWW, true));
+events.forEach(e => window.addEventListener(e, startNOWWW, true) );
